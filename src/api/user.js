646450b2a05816aac98658b29546c7a849e43d39
jiange <1563610087@qq.com>
2021-07-14 18:53:24 +0800

    验证码

--- src/api/user.js
+++ src/api/user.js
@@ -2,3 +2,4 @@ import request from '@/common/js/request'
 
 export const login = (data) => request.post('/user/login', data)
 export const logout = (data) => request.post('/data/allSites', data)
+export const getCode = () => request.get('/login/code')
